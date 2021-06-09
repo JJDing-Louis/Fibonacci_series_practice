e21241c0392305aeec669ff670a7c05610d7ffd6
J.J.Ding <louis7012161@gmail.com>
2021-06-10 01:16:39 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Start coding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fibonacci_series_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
